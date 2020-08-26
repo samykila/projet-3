7cd534249155171be07e3eac5fb81f2db9bf0afe
Samyna <samyna.nourdine@hotmail.com>
2020-08-26 13:37:51 +0200

    appnext

--- js/app.js
+++ js/app.js
@@ -1,18 +1,82 @@
 
+// recuperation des stations + creation des marqueurs
 
-   
-        // creation des boucles pour les différentes stations
-        for (station in response) {
-            let  marker = new L.marker([response[station].position.lat, response[station].position.lng]);// pas de addTo(macarte), l'affichage sera géré par la bibliothèque des clusters
+ajaxGET("https://api.jcdecaux.com/vls/v1/stations?contract=lyon&apiKey=a1bb8a2d6bce79ce80f96e68686478140bfc9616");
+function (response) {
+    let butRes = document.getElementById("butReservation");
+    const listeStations = JSON.parse(response);
 
-            const elt = document.getElementById('formulaire');    // On récupère l'élément sur lequel on veut détecter le clic
-            marker.addEventListener('click', function() {
-                lastation = response[station];
-                console.log(lastation.name);   // On écoute l'événement click
-                let form = document.querySelector("form");
-                let station = document.getElementById('station').value = lastation.name;
-                let address = document.getElementById('address').value = lastation.address;
-                let disponibility = document.getElementById('disponibility').value = lastation.disponibility;
+listeStations.forEach(station => {
+    let myGreenIcon = L.icon({
+        iconUrl: "images/greenMark.png",
+        iconSize: [50, 50],
+        iconAnchor: [25, 50],
+        popupAnchor: [-3, -76],
+    });
 
-            });
-            
+    let  myRedIcon = L.icon({
+        iconUrl: "images/redMark.png",
+        iconSize: [50, 50],
+        iconAnchor: [25, 50],
+        popupAnchor: [-3, -76],
+    });
+
+    let  marker ="";
+    if (station.status === "OPEN") {
+        marker = L.marker([station.position.lat,station.position.lng], { icon: myGreenIcon }).addTo(carte);
+    }
+    else {
+        marker = L.marker([station.position.lat,station.position.lng], { icon: myRedIcon }).addTo(carte);
+    }
+
+    //Affichage des infos de la station lors d'un clic sur son marker
+    marker.addEventListener("click", function() {
+        document.getElementById("infoStations").classList.remove("hid");
+        document.getElementById("form").style.display = "none";
+        let address = document.getElementById("adresseStation");
+        let  state = document.getElementById("etatStation");
+        let  avail = document.getElementById("veloDispo");
+        let  space = document.getElementById("attacheDispo");
+        let  stateStation = "";
+
+        if (station.status === "OPEN") {
+            stateStation = "Station ouverte";
+        }
+        else {
+            stateStation = "Station fermée";
+        }
+
+        address.innerHTML = "";
+        state.innerHTML= "";
+        avail.innerHTML= "";
+        space.innerHTML= "";
+
+        address.innerHTML += station.address;
+        state.innerHTML += stateStation;
+        avail.innerHTML += station.available_bikes;
+        space.innerHTML += station.available_bike_stands;
+
+        //Nettoyage + Mise en mémoire de l'adresse de la station sélectionnée
+        sessionStorage.clear();
+        sessionStorage.setItem("stationAdress", station.address);
+
+        //Vidage du canvas si déjà précédemment rempli
+        canvas.clearCanvas();
+
+        //Affichage du bouton permettant de réserver
+        butRes.classList.remove("hid");
+        butRes.style.display = "block";
+        //Remplacement du bloc 'Info de la station' par le formulaire de réservation lors du clic sur le bouton réserver
+        butRes.addEventListener("click", function(){
+            document.getElementById("form").style.display = "flex";
+            document.getElementById("infoStations").classList.add("hid");
+
+            //Pré-remplissage des champs nom/prénom si une réservation a déjà été faite précédemment
+            if(reservation.nom !== "") {
+                document.getElementById("inputLastName").value = reservation.storedName;
+                document.getElementById("inputFirstName").value = reservation.storedFName;
+             }
+         });
+     });
+  });
+}
