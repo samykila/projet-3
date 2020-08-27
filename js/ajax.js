7bf9ebfc4c8026aea3c41657154410c59cfbbdd1
Samyna <samyna.nourdine@hotmail.com>
2020-08-27 14:09:32 +0200

    remove addEvent Listener

--- js/ajax.js
+++ js/ajax.js
@@ -2,24 +2,16 @@
 //strict mode
 "use strict"
 // on recupère les données
-function ajaxGet(url, callback) {
+function ajaxGet(url, callback){
     let request = new XMLHttpRequest();
     request.open("GET", url);
 
     request.addEventListener("load", function () {
         if (request.status >= 200 && request.status < 400) { // traitement de la requete réusssi
             callback(request.responseText);
-        }
-        else {
+        } else {
             // Affichage des informations sur l'échec du traitement de la requête
             console.error(request.status + " " + request.statusText);
         }
-    });
-
-    request.addEventListener("error", function () {
-        // La requête n'a pas réussi à atteindre le serveur
-        console.error("Erreur réseau");
-    });
-
-    request.send(null);
+    })
 }
