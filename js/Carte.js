6e47d0cc3b6f72add5912ec7372c912dfc0827ff
Samyna <samyna.nourdine@hotmail.com>
2020-08-27 14:15:57 +0200

    change url for init map from openstreetmap to mapbox

--- js/Carte.js
+++ js/Carte.js
@@ -3,23 +3,23 @@
 "use strict";
 
 class Carte {
-    constructor( lat, lng) {
-        this.lat=lat;
-        this.lng=lng;
+    constructor (lat, lng) {
+        this.lat = lat;
+        this.lng = lng;
     }
 
 // on initialise la carte
-    initCarte() {
+    initCarte () {
         let carte = L.map('macarte').setView([this.lat, this.lng], 10);
 
         // on charge les tuiles pour afficher la carte
-        L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-            attribution: 'Map data &copy; <a href="https://www.openstreetmap.fr/">OpenStreetMap</a> contributors,<a href="https://creativecommons.org/licenses/by-sa/2.0/"></a>',
-            minZoom: 1,
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            minZoom: 2,
             maxZoom: 20,
-        })
-        L.addTo(carte);
+        }).addTo(carte);
     }
+}
 
 
 
